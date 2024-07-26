e316723f5e7aa23e7ec4559c2c6c671a517762ae
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-26 10:19:46 +0200

    Implemented basic react router dom for routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Navbar.css"
 
 export default function Navbar() {
   return (
     <header className="header">
-        <div className="logo-container">
-            <h1>Task Management</h1>
-        </div>
-        <nav>
-            <button>Home</button>
-            <button>All Tasks</button>
-            <button>New Task</button>
-            <button>All Categories</button>
-            <button>Create Category</button>
-        </nav>
+      <div className="logo-container">
+        <h1>Task Management</h1>
+      </div>
+      <nav>
+        <Link to="/"><button>Home</button></Link>
+        <Link to="/all-tasks"><button>All Tasks</button></Link>
+        <button>All Tasks</button>
+        <button>New Task</button>
+        <button>All Categories</button>
+        <button>Create Category</button>
+      </nav>
     </header>
   )
 }
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import Home from "./Home"
-import AllTasks from './AllTasks'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Navbar from '../components/Navbar/Navbar';
+import Home from "./Home";
+import AllTasks from './AllTasks';
 
 export default function Layout() {
     return (
-        <main>
+        <Router>
             <Navbar />
-            <>
-                {/* <Home /> */}
-                <AllTasks />
-            </>
-        </main>
-    )
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/all-tasks" element={<AllTasks />} />
+            </Routes>
+        </Router>
+    );
 }
