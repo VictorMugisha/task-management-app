c8c8f76cf6dced5c87e14cbc92f979c6593924af
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-26 13:43:18 +0200

    Added delete category functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,25 @@ import useLocalStorage from "./hooks/useLocalStorage"
 
 export default function App() {
 
+  // Tasks Context
   const allTasks = useLocalStorage("allTasks")
 
   const tasksContextValue = {
     allTasks
   }
 
+
+  // Categories Context
   const allCategories = useLocalStorage("allCategories")
 
+  function deleteCategory(categoryId) {
+    const newCategories = allCategories.filter(category => category.categoryId !== categoryId)
+    localStorage.setItem("allCategories", JSON.stringify(newCategories))
+  }
+
   const categoriesContextValue = {
-    allCategories
+    allCategories,
+    deleteCategory
   }
 
   return (
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CategoriesContext from '../../contexts/CategoriesContext'
 import "./Category.css"
 
-export default function Category({category}) {
+export default function Category({ category }) {
+    const { deleteCategory } = useContext(CategoriesContext)
     return (
         <div className="category">
             <h3 className="category-title">{category.categoryTitle}</h3>
             <div className="buttons">
-                <button className="category-button">Edit</button>
-                <button className="category-button">Delete</button>
+                <button
+                    className="category-button"
+                >
+                    Edit
+                </button>
+                <button
+                    className="category-button"
+                    onClick={() => deleteCategory(category.categoryId)}
+                >
+                    Delete
+                </button>
             </div>
         </div>
     )
