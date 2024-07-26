d665f7a46521c5999e973b3b8966e84e16ca844a
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-26 13:49:00 +0200

    Added functionality to load tasks from global context

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./Task.css"
 
-export default function Task() {
+export default function Task({task}) {
     return (
         <div className="task">
-            <p className="task-title">Learning React Higher Order Components</p>
-            <p className="task-category">Personal</p>
-            <p className="due-date">12 Nov 2024, 12:02 PM</p>
-            <p className="completion-status">Pending</p>
+            <p className="task-title">{task.taskTitle}</p>
+            <p className="task-category">{task.taskCategory}</p>
+            <p className="due-date">{task.dueDate}</p>
+            <p className="completion-status">{task.isComplete ? "Complete" : "Pending"}</p>
 
             <div className="buttons">
                 <button>Complete</button>
--- src/pages/AllTasks.jsx
+++ src/pages/AllTasks.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import TasksContext from '../contexts/TasksContext'
 import Task from '../components/Task/Task'
 
 // Stylesheet for this component are in Task folder in components
 
 export default function AllTasks() {
+    const { allTasks } = useContext(TasksContext)
     return (
         <section className="all-tasks-container">
             <h1 className="title">All Tasks</h1>
             <div className="container">
-                <Task />
-                <Task />
-                <Task />
-                <Task />
+                {
+                    allTasks.length ?
+                    allTasks.map(task => (
+                        <Task key={task.taskId} task={task} />
+                    )) :
+                    <div>No Tasks Have Been Saved Yet!!!</div>
+                }
             </div>
         </section>
     )
