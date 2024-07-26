2aca125ab4048e63219406881a064775ec67fc26
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-26 12:29:30 +0200

    Implemented CategoriesContext and TasksContext to manage global state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from "react"
 import './App.css'
 import Layout from "./pages/Layout"
+import TasksContext from "./contexts/TasksContext"
+import CategoriesContext from "./contexts/CategoriesContext"
+import useLocalStorage from "./hooks/useLocalStorage"
 
 export default function App() {
-  
+
+  const allTasks = useLocalStorage("allTasks")
+
+  const tasksContextValue = {
+    allTasks
+  }
+
+  const allCategories = useLocalStorage("allCategories")
+
+  const categoriesContextValue = {
+    allCategories
+  }
+
   return (
-    <Layout />
+    <TasksContext.Provider value={tasksContextValue}>
+      <CategoriesContext.Provider value={categoriesContextValue}>
+        <Layout />
+      </CategoriesContext.Provider>
+    </TasksContext.Provider>
   )
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import TasksContext from '../../contexts/TasksContext'
+import CategoriesContext from '../../contexts/CategoriesContext'
 import "./Dashboard.css"
 
 export default function Dashboard() {
-  return (
-    <section>
-        <div className="dashboard-title">
-            <h3>Dashboard</h3>
-        </div>
-        <div className="dashboard-cards">
-            <div className="card">
-                <h1>12</h1>
-                <p>Tasks</p>
+    const { allTasks } = useContext(TasksContext)
+    const { allCategories } = useContext(CategoriesContext)
+    return (
+        <section>
+            <div className="dashboard-title">
+                <h3>Dashboard</h3>
             </div>
-            <div className="card">
-                <h1>5</h1>
-                <p>Categories</p>
+            <div className="dashboard-cards">
+                <div className="card">
+                    <h1>{allTasks.length}</h1>
+                    <p>Tasks</p>
+                </div>
+                <div className="card">
+                    <h1>{allCategories.length}</h1>
+                    <p>Categories</p>
+                </div>
+                <div className="card">
+                    <h1>6</h1>
+                    <p>Tasks <span>Completed</span></p>
+                </div>
+                <div className="card">
+                    <h1>4</h1>
+                    <p>Tasks <span>Pending</span></p>
+                </div>
             </div>
-            <div className="card">
-                <h1>6</h1>
-                <p>Tasks <span>Completed</span></p>
-            </div>
-            <div className="card">
-                <h1>4</h1>
-                <p>Tasks <span>Pending</span></p>
-            </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
