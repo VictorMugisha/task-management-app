4736b3d29938838ead49233d8307bc8f1d94d5a9
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-26 12:33:47 +0200

    Updated Categories to render registered Categories

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./Category.css"
 
-export default function Category() {
+export default function Category({category}) {
     return (
         <div className="category">
-            <h3 className="category-title">Personal</h3>
+            <h3 className="category-title">{category.categoryTitle}</h3>
             <div className="buttons">
                 <button className="category-button">Edit</button>
                 <button className="category-button">Delete</button>
--- src/pages/AllCategories.jsx
+++ src/pages/AllCategories.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CategoriesContext from "../contexts/CategoriesContext"
 import Category from '../components/Category/Category'
 
 export default function AllCategories() {
+  const { allCategories } = useContext(CategoriesContext)
   return (
     <section className="categories-container">
-        <h2 className="title">Registered Categories</h2>
-        <div className="container">
-            <Category />
-            <Category />
-            <Category />
-        </div>
+      <h2 className="title">Registered Categories</h2>
+      <div className="container">
+        {
+          allCategories.length ?
+          allCategories.map(category => (
+            <Category key={category.categoryId} category={category} />
+          )) :
+          <div>No Categories Added Yet!</div>
+        }
+      </div>
     </section>
   )
 }
