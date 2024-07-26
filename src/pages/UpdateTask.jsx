4aa2f002d544b443224eba6374718832f22eca8f
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-26 16:19:49 +0200

    Now tasks can be updated

--- src/pages/UpdateTask.jsx
+++ src/pages/UpdateTask.jsx
@@ -1,37 +1,92 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import TasksContext from '../contexts/TasksContext'
 import CategoriesContext from '../contexts/CategoriesContext'
 import "../otherStyles/NewTask.css"
 
 export default function UpdateTask() {
+    
     const { taskId } = useParams()
     const { allTasks } = useContext(TasksContext)
-
+    
     const taskToUpdate = allTasks.find(task => task.taskId === taskId)
 
     const { allCategories } = useContext(CategoriesContext)
+    
+    const [updateTaskFormData, setUpdateTaskFormData] = useState(taskToUpdate)
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setUpdateTaskFormData(prevData => ({
+            ...prevData,
+            [name]: value
+        }))
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        if (updateTaskFormData.taskTitle == "") {
+            alert("Form Title is Required!!")
+            return
+        }
+
+        function getCurrentDateFormatted() {
+            const now = new Date();
+            const year = now.getFullYear();
+            const month = String(now.getMonth() + 1).padStart(2, '0');
+            const day = String(now.getDate()).padStart(2, '0');
+          
+            return `${year}-${month}-${day}`;
+          }
+          
+          console.log(getCurrentDateFormatted());
+          
+
+        if (updateTaskFormData.dueDate == "") {
+            updateTaskFormData.dueDate = getCurrentDateFormatted()
+        }
+
+        const dataToSave = allTasks.map(task => {
+            if (task.taskId === taskId) {
+                return updateTaskFormData
+            } else {
+                return task
+            }
+        })
+
+        localStorage.setItem("allTasks", JSON.stringify(dataToSave))
+
+        setUpdateTaskFormData({
+            taskId: '',
+            taskTitle: '',
+            taskCategory: '',
+            dueDate: '',
+            isComplete: false,
+        })
+
+        alert("Task is Updated Successfuly!!")
+    }
     return (
         <section className="new-task-container">
             <h2 className="title">Update Task</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="input">
                     <p>Enter Task Title</p>
                     <input
                         type="text"
                         placeholder="Task Title"
                         name="taskTitle"
-                        value={taskToUpdate.taskTitle}
-                    // onChange={}
+                        value={updateTaskFormData.taskTitle}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="input">
                     <p>Selece Task Category</p>
                     <select
                         name="taskCategory"
-                    // onChange={}
+                        onChange={handleChange}
                     >
-                        <option value={taskToUpdate.taskCategory}>{taskToUpdate.taskCategory}   </option>
+                        <option value={updateTaskFormData.taskCategory}>{updateTaskFormData.taskCategory}   </option>
                         {
                             allCategories.length ?
                                 allCategories.map(category => (
@@ -46,8 +101,8 @@ export default function UpdateTask() {
                     <input
                         type="date"
                         name="dueDate"
-                        value={taskToUpdate.dueDate}
-                    // onChange={}
+                        value={updateTaskFormData.dueDate}
+                        onChange={handleChange}
                     />
                 </div>
                 <button
